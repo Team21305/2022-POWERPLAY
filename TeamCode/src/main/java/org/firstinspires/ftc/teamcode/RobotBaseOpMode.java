6207221ec1d87cf99a79886ba02d8cd1226e0edd
Nick Manor <nmanor@rbaconsulting.com>
2022-11-14 09:16:53 -0600

    Create RobotBase for all OpModes to inherit.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotBaseOpMode.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.button.Button;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeSubSystem;
+import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.VisionSubSystem;
+
+public class RobotBaseOpMode extends CommandOpMode {
+
+    public GamepadEx gamepad;
+    public GamepadEx gamepadCo;
+
+    public DriveSubsystem driveSubsystem;
+    public LiftSubsystem liftSubsystem;
+    public IntakeSubSystem intakeSubsystem;
+    public VisionSubSystem visionSubSystem;
+
+    public Button downButton;
+    public Button upButton;
+    public Button openButton;
+    public Button closeButton;
+    public Button middleButton;
+    public Button groundButton;
+    public Button lowButton;
+    public Button auto;
+    public Button auto2;
+    public Button auto3;
+
+
+    @Override
+    public void initialize() {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        MultipleTelemetry multipleTelemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        telemetry = multipleTelemetry;
+
+        Hardware hardware = new Hardware(hardwareMap);
+        driveSubsystem = new DriveSubsystem(hardware, multipleTelemetry);
+        liftSubsystem = new LiftSubsystem(hardware, multipleTelemetry);
+        intakeSubsystem = new IntakeSubSystem(hardware, multipleTelemetry);
+        visionSubSystem = new VisionSubSystem(hardware, multipleTelemetry);
+
+        gamepad = new GamepadEx(gamepad1);
+        gamepadCo = new GamepadEx(gamepad2);
+
+        closeButton = (new GamepadButton(gamepadCo, GamepadKeys.Button.A));
+        openButton = (new GamepadButton(gamepadCo, GamepadKeys.Button.Y));
+        lowButton = (new GamepadButton(gamepadCo, GamepadKeys.Button.X));
+        groundButton = (new GamepadButton(gamepadCo, GamepadKeys.Button.DPAD_RIGHT));
+        middleButton = (new GamepadButton(gamepadCo, GamepadKeys.Button.DPAD_LEFT));
+        upButton = (new GamepadButton(gamepadCo, GamepadKeys.Button.DPAD_UP));
+        downButton = (new GamepadButton(gamepadCo, GamepadKeys.Button.DPAD_DOWN));
+
+        auto = (new GamepadButton(gamepad, GamepadKeys.Button.DPAD_LEFT));
+        auto2 = (new GamepadButton(gamepad, GamepadKeys.Button.DPAD_UP));
+        auto3 = (new GamepadButton(gamepad, GamepadKeys.Button.DPAD_RIGHT));
+
+        register(driveSubsystem, liftSubsystem, intakeSubsystem, visionSubSystem);
+
+
+
+
+
+
+    }
+}
