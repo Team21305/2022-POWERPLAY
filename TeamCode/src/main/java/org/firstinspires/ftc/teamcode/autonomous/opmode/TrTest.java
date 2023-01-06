df008b34d664fe9080b9535924c1c47131d27d9e
Nick Manor <nmanor@rbaconsulting.com>
2023-01-06 10:06:32 -0600

    -More left side tuning (Q2, Q3).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmode/TrTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/opmode/TrTest.java
@@ -220,15 +220,81 @@ public class TrTest extends LinearOpMode {
         Pose2d startPose2 = new Pose2d(0, 0, Math.toRadians(90));
 
         drive.setPoseEstimate(startPose2);
+
+        // Vectors
+        double flipOverX = 1;
+        double medJunXOffset = 0;
+
+        Vector2d coneStackPre = new Vector2d(-56, -12 * flipOverX);
+        Vector2d coneStack = new Vector2d(-58.5, -12 * flipOverX);
+        Vector2d terminal = new Vector2d(-56, -60 * flipOverX);
+        Vector2d lowJunction = new Vector2d(-46, -12 * flipOverX);
+        Vector2d mediumJunction = new Vector2d(-22+medJunXOffset, -12 * flipOverX);
+        Vector2d readSignal = new Vector2d(-30, -60* flipOverX);
+
+        Vector2d park = new Vector2d(-12, -12 * flipOverX);
+
+        double right90 = Math.toRadians(-90 * flipOverX);
+        double left90 = Math.toRadians(90 * flipOverX);
+        Pose2d startPose3 = new Pose2d(-31, -63 * flipOverX, Math.toRadians(90 * flipOverX));
+
+        drive.setPoseEstimate(startPose3);
+
+        // written from Red Left
         TrajectorySequence poseTest3 = drive
-                .trajectorySequenceBuilder(startPose2)
+                .trajectorySequenceBuilder(startPose3)
+
+                // read signal
+                .lineToLinearHeading(new Pose2d(readSignal, Math.toRadians(90 * flipOverX)))
+                .waitSeconds(.1)
+
+                // To Terminal
+                .turn(left90)
+                .lineToLinearHeading(new Pose2d(terminal, Math.toRadians(180)))
+                // drop cone
+                .addTemporalMarker(()->intake.open())
+                .waitSeconds(.2)
+                .turn(right90)
+
+                // To Cone Stack
+                .lineToLinearHeading(new Pose2d(coneStackPre, Math.toRadians(90 * flipOverX)))
+                .addTemporalMarker(()->lift.goToPosition(0.18))
+                .turn(left90)
+                .lineToLinearHeading(new Pose2d(coneStack, Math.toRadians(180)))
+
+                // grab cone
                 .addTemporalMarker(()->intake.close())
-                .addTemporalMarker(.3, ()->lift.goToBottom())
+                .waitSeconds(0.4)
+                .addTemporalMarker( ()->lift.goToLow())
 
-                .lineToLinearHeading(new Pose2d(0, multiplier * 22, Math.toRadians(multiplier * 90)))
+                // To Low Junction
+                .lineToLinearHeading(new Pose2d(lowJunction, Math.toRadians(180)))
+                .turn(left90)
+                // Drop Cone
+                .addTemporalMarker(()->intake.open())
 
+
+                // To Cone Stack
+                .turn(right90)
+                .addTemporalMarker(()->lift.goToPosition(0.14))
+                .lineToLinearHeading(new Pose2d(coneStack, Math.toRadians(180)))
+                // grab cone
                 .addTemporalMarker(()->intake.close())
+                .waitSeconds(0.4)
+                .addTemporalMarker( ()->lift.goToLow())
 
+                // To Medium Junction
+                .lineToLinearHeading(new Pose2d(mediumJunction, Math.toRadians(180)))
+                .addTemporalMarker(()->lift.goToMiddle())
+                .turn(left90)
+                // Drop Cone
+                .addTemporalMarker(()->intake.open())
+                .waitSeconds(.2)
+
+                // Park
+                .lineToLinearHeading(new Pose2d(park, Math.toRadians(90)))
+                //.turn(right90)
+                .waitSeconds(.2)
 
                 .build();
 
